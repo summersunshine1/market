2fc80881d6e2e8dfb10ac6ffa10040a331c2e594
summersunshine1 <summersunshine366@gmail.com>
2017-07-28 16:11:03 +0800

    add param

--- xgboosttrain.py
+++ xgboosttrain.py
@@ -20,11 +20,11 @@ def train_model():
     dtrain = xgb.DMatrix(train_path)
     dtest = xgb.DMatrix(test_path)
     param = {'max_depth':5,'verbose':True,'min_child_weight':1,'gamma':0,'n_estimators':1000,
-    'subsample':0.8, 'colsample_bytree':0.8}
+    'subsample':0.8, 'colsample_bytree':0.8,'scale_pos_weight':6.7,'nthread':4}
     num_round=100
     watchlist = [(dtest,'eval'), (dtrain,'train')]
     bst = xgb.train(param, dtrain, num_round,watchlist)
-    bst.save_model(pardir+'/model/xgb.model')
+    bst.save_model(pardir+'/model/xgb1.model')
     print(bst)
     y_pred = bst.predict(dtest)
     print(f1_score1(y_pred, dtest))
